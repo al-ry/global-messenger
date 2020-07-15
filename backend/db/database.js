c68ab821bcf7988d3e4edb4507d56a73a76b0a06
al-ry <saska_ryb@mail.ru>
2020-07-15 12:46:39 +0300

    added db

---
+++ backend/db/database.js
@@ -0,0 +1,11 @@
+var sqlite3 = require('sqlite3').verbose();
+
+var db = new sqlite3.Database('test.sqlite3');
+console.log('yes');
+
+
+
+db.serialize(function() {
+    var prepare = db.prepare('INSERT INTO test(name) VALUES (?)');
+    prepare.run('Alexander');
+})
