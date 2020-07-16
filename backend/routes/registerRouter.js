b2b8f5aded4563804500ce406ee0a0425332ae04
al-ry <saska_ryb@mail.ru>
2020-07-16 13:04:58 +0300

    updated

--- backend/routes/registerRouter.js
+++ backend/routes/registerRouter.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 const registerRouter = Router()
+const registerController = require('../controllers/registerController.js')
+var bodyParser = require('body-parser')
 
 registerRouter.post('/register', (req, res) => {
-
+    registerController.ParseUserData(req, res)
 })
 
 module.exports = registerRouter
