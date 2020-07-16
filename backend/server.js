88da614df51343b65ad5dfc4ad14a714e2e81866
GreatHorizon <52240672+GreatHorizon@users.noreply.github.com>
2020-07-16 13:42:46 +0300

    Changed post request

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,26 @@
 var db = require('./db/database.js')
 
-
 var express = require('express')
 var app = express()
 var server = require('http').createServer(app)
 var crypto = require('crypto')
 var uuid = require('uuid')
 var bodyParser = require('body-parser')
+
 var registerRouter = require('./routes/registerRouter')
 var loginRouter = require('./routes/loginRouter')
 
 app.use(bodyParser.json())
+app.use(require('connect').bodyParser());
 app.use(bodyParser.urlencoded({extended : true}))
+
 app.use(registerRouter)
 app.use(loginRouter)
 
 var GetRandomString = function(length) {
     return crypto.randomBytes(Math.ceil(length/2)).toString('hex').slice(0, length)
 }
+
 var SHA512 = function(password, salt)
 {
     var hash = crypto.createHmac('sha512', salt)
@@ -28,6 +31,7 @@ var SHA512 = function(password, salt)
         passwordHash : value
     }
 }
+
 function SaltHashPassword(userPassword){
     var salt = GetRandomString(16)
     var passwordData = SHA512(userPassword, salt)
