1d26dde62f0ae22a92360bb61a031e0787afa8ca
al-ry <saska_ryb@mail.ru>
2020-07-27 00:00:22 +0300

    can get chats list by number and add chat by number

--- backend/controllers/homeController.js
+++ backend/controllers/homeController.js
@@ -1,4 +1,3 @@
-var User = require('../models/user.js')
 
 exports.GetHome = (req, res) => {
     console.log('================home')
@@ -7,13 +6,7 @@ exports.GetHome = (req, res) => {
     console.log(req.session.user)   
     console.log('================home')
     if (req.session.user) {       
-        console.log(req.session.user)
-        var user = {
-            id_user: req.session.user.id_user,
-            name: req.session.user.name,
-            telephone: req.session.user.telephone
-        }
-        res.status(200).json(user)
+        res.status(200).send('success')
     } else {
         res.status(400).send('fail')
     }
