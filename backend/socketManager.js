629fddee5b0d7882d15fad64bf7a6676c4596613
al-ry <saska_ryb@mail.ru>
2020-07-30 16:59:42 +0300

    stored messages

--- backend/socketManager.js
+++ backend/socketManager.js
@@ -9,41 +9,51 @@ module.exports = function(socket) {
 
     console.log('User connected:' + socket.id)
 
-    // connectedUsers.push(socket.id)
-    // console.log(connectedUsers)
+    //connectedUsers.push(socket.id)
+    console.log(connectedUsers)
 
     socket.on('user_connected', (userPhone, cookie) => {
+        console.log('=======user_connected=======')
         if (usersCookies[userPhone] != undefined)
         {
-            io.emit('log_out')
+            console.log('=======user_connected if=======')
+            io.to(connectedUsers[userPhone]).emit('log_out')
             var sessionId = GetSessionId(usersCookies[userPhone])
-            sessionDB.get(sessionId, (error, session) => {
-                console.log(session)
-            })
-            sessionDB.destroy(sessionId, (err) => {
-                if (err) throw err;
-            })
+            console.log("logging out user " + userPhone + "with sessionId " + sessionId)
+            sessionDB.destroy(sessionId)
+            console.log('=======user_connected if=======')
         }
+
         usersCookies[userPhone] = cookie
         connectedUsers[userPhone.toString()] = socket.id
         console.log(connectedUsers)
+        console.log(usersCookies)
+        console.log('=======user_connected=======')
     })
 
 
     socket.on('send_message', (sender, receiver, msg) => {
-        if(connectedUsers[data.receiver] != undefined)
+        if(connectedUsers[receiver] != undefined)
         {
-            //newMessage = new Message(data.sender, data.receiver, data.msg, data.date)
+            newMessage = new Message(data.sender, data.receiver, data.msg, data.date)
             var receiverSocketId = connectedUsers[receiver]
             io.to(receiverSocketId).emit("new_message", msg)
         }
-        //newMessage.Save()
+        newMessage.Save()
         //add to db
     })
     socket.on('disconnection', (userPhone) => {
-        delete connectedUsers[userPhone]    
+        console.log('=======disconnection=======')
         console.log(connectedUsers)
-        console.log('User disconnected ' + socket.id)   
+        console.log(connectedUsers[userPhone]) 
+        console.log(usersCookies)
+        console.log(usersCookies[userPhone]) 
+        delete connectedUsers[userPhone]   
+        delete usersCookies[userPhone]
+        console.log(connectedUsers[userPhone]) 
+        console.log(connectedUsers)
+        console.log('User disconnected ' + socket.id + "," + userPhone)  
+        console.log('=======disconnection=======') 
     })
 }
 
