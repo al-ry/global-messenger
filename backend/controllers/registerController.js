56612db940622327d712713db36e7719e347f754
al-ry <saska_ryb@mail.ru>
2020-08-03 15:07:43 +0300

    changed  name of variable

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -9,8 +9,8 @@ exports.Register = (req, res) => {
             res.status(400).send('login is busy')
         } else {
             newUser.Register()
-            User.FindOne(newUser.telephone).then(newResult => {
-                req.session.user = newResult
+            User.FindOne(newUser.telephone).then(newUser=> {
+                req.session.user = newUser
                 var newCookie = cookieUtil.SignCookie(req.sessionID);
                 req.cookies['connect.sid'] = newCookie
                 res.status(200).json(req.cookies)
